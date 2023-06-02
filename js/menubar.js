94d4d83dac73bd20d4175a15e8600d32f48f434f
SeeeeHyeon <132569606+SeeeeHyeon@users.noreply.github.com>
2023-06-03 01:22:49 +0900

    Delete js directory

--- js/menubar.js
+++
@@ -1,9 +0,0 @@
-
-const bars = document.querySelector(".fas");
-const menu = document.querySelector("#menu");
-
-function onClick() {
-  menu.classList.toggle("hidden");
-}
-
-bars.addEventListener("click", onClick)
